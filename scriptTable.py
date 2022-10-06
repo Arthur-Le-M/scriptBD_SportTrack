8a850292a8c709dc416e11dbebbd6734215ae736
Arthur Le Menn <arthurlemenn29870@gmail.com>
2022-10-06 23:19:06 +0200

    Possibilité de planifier tous les matchs d'un championnat terminé

--- scriptTable.py
+++ scriptTable.py
@@ -163,7 +163,6 @@ def round_robin(idEquipe):
     return planification
 
 def planifierMatch(idChampionnat, debutSaison):
-    listeRencontre = []
     #Récupérer les idEquipe que l'on va stocker dans un tableau
     listeIdEquipe = []
     query = 'SELECT id_equipe FROM participerChampionnat WHERE id_championnat = %s'
@@ -189,15 +188,54 @@ def planifierMatch(idChampionnat, debutSaison):
     #Gerer la date sous le format DATETIME : 1000-01-01 00:00:
     dicoDate = {}
     dateActuelle = debutSaison
-    for i in range(len(planification)):
+    journee = 0
+    while journee < len(planification):
         dateActuelle += datetime.timedelta(weeks=1)
+        print(journee, dateActuelle < datetime.date(dateActuelle.year, 12, 15) and dateActuelle > datetime.date(dateActuelle.year, 1, 10))
         if dateActuelle < datetime.date(dateActuelle.year, 12, 15) and dateActuelle > datetime.date(dateActuelle.year, 1, 10):
-            dicoDate[i+1] = dateActuelle
-    print(dicoDate)
+            dicoDate[journee] = dateActuelle
+            journee += 1
+
+    #Insérer la planification dans la table match
+
+    for i in range(len(planification)-1):
+        print(len(planification), len(dicoDate))
+        dateJournee = dicoDate[i]
+        print(dateJournee)
+        for j in range(len(planification[i])):
+            #On récupère toutes les informations que l'on a besoin
+            #On récupère les équipes domicile et extérieur
+            idEquipeDom = planification[i][j][0] 
+            idEquipeExt = planification[i][j][1]
+            #On récupère les horaires et la date
+            heureDebut = datetime.datetime(dateJournee.year, dateJournee.month, dateJournee.day, 14, 30, 00)
+            heureFin = datetime.datetime(dateJournee.year, dateJournee.month, dateJournee.day, 16, 15, 00)
+            #On récupère le stade de l'équipe domicile
+            query = "SELECT id_stade FROM Equipe WHERE id = %s"
+            val = [idEquipeDom]
+            cursor.execute(query, val)
+            record = cursor.fetchall()
+            idStade = record[0][0]
+            #On créer l'id
+            while True:
+                idMatch = generateur.genRandomID(5)
+                query = "SELECT COUNT(*) FROM MatchTable WHERE id = %s"
+                val = [idMatch]
+                cursor.execute(query, val)
+                record = cursor.fetchall()
+                if record[0][0] == 0:
+                    break
+            #Les info sont récupérer on insère dans la base
+            query = """
+            INSERT INTO MatchTable (id, jouer, heure_debut, heure_fin, id_equipe_dom, id_equipe_ext, id_stade, journee)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+            """
+            val = [idMatch, 0, heureDebut, heureFin, idEquipeDom, idEquipeExt, idStade, i+1]
+            cursor.execute(query, val)
+            conn.commit()
 
 
-
-planifierMatch(36949, datetime.date(2022, 8, 28))
+planifierMatch(27043, datetime.date(2022, 8, 28))
 
 '''dateDebut = datetime.date(2022, 8, 28)
 dateFin = datetime.date(2023, 6, 11)
