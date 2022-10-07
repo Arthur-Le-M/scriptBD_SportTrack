5b7e0cf4c8ca827adbf0f5840895cc8a3d6a52f2
Arthur-Le-M <arthurlemenn29870@gmail.com>
2022-10-07 11:27:47 +0200

    Ajout de la fonction 'evenementAleatoire' qui génère un type d'évènement aléatoire

--- generateur.py
+++ generateur.py
@@ -179,4 +179,15 @@ def remplirChampionnat(championnat):
         joueurs.append(gen[2])
     return (participerChampionnat, equipe, stade, joueurs)
 
+
+#Simulation de match
+def evenementAleatoire():
+    evenementPossible = ['BUT', 'CARTON_JAUNE', 'CARTON_ROUGE']
+    i = random.randint(0, len(evenementPossible)-1)
+    return evenementPossible[i]
+
+
+for i in range(9):
+    print(evenementAleatoire())
+
 print("Chargement terminé")
